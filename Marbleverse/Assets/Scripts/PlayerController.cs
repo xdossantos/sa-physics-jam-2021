ee4e29c46acc25848e11265fd8e845a7073ce831
xdossantos <xola.dossantos@xdode.com>
2021-08-21 17:39:59 +0200

    Added update method to player script

--- Marbleverse/Assets/Scripts/PlayerController.cs
+++ Marbleverse/Assets/Scripts/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
 
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero; 
-    float rotationX = 0; i
+    float rotationX = 0; 
 
     [HideInspector]
     public bool canMove = true;
@@ -34,6 +34,48 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        // We are grounded so we need to recalculate move direction based on axes
+        Vector3 forward = transform.TransformDirection(Vector3.forward); 
+        Vector3 right = transform.TransformDirection(Vector3.right); 
+
+        // Press Left shift to run 
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift); 
+        float curSpeedX = canMove? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical"): 0; 
+        float curSpeedY = canMove? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal"): 0; 
+        float movementDirectionY = moveDirection.y; 
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY); 
+
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        {
+            moveDirection.y = jumpSpeed; 
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY; 
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below when the moveDirection is multiplied by deltaTime). 
+        // This is because gravity should be appled as an acceleration (ms^-2)
+
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime; 
+        }
+
+        // Move the controller
+
+        characterController.Move(moveDirection *  Time.deltaTime); 
+
+        // Player and Camera rotation should
+        if(canMove)
+        {
+            rotationX += Input.GetAxis("Mouse Y") * lookSpeed; 
+            rotationX = Math.Clamp(rotationX, -lookXLimit, lookXLimit); 
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0); 
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0); 
+        }
+
     }
 }
