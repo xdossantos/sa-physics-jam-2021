23f1d08a10eb50c23318e5b74b080f2eb5f50dbc
Regina <rkgatle@mfsafrica.com>
2021-08-22 16:47:17 +0200

    basi skeleton
    basicskeleton

---
+++ MAebleverse2.0/Assets/scripts/camera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+
+    public GameObject target;
+    public float xOffset, yOffset, zOffset;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position + new Vector3(xOffset,yOffset,zOffset);
+        transform.LookAt(target.transform.position);
+    }
+}
